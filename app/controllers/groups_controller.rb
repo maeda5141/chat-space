6d1bbb8aebed143f5b3a04295fd519904af7a22a
maeda5141 <maedak514@gmail.com>
2019-06-05 02:52:00 +0900

    Update chat-space
    グループ機能の追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,37 @@
 class GroupsController < ApplicationController
+  def index
+    
+  end
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+  def create
+    @group = Group.new(group_params)
+    @save = @group.save
+    if @save
+    flash[:success] = "グループを作成しました"
+    redirect_to root_path
+    else
+      @group.valid?
+      render :new
+    end
+  end
+  def edit
+    @group = Group.find(params[:id])
+  end
+  def update
+    if Group.update(group_params)
+      flash[:success] = "編集が完了しました"
+      redirect_to root_path
+      else
+        @group.valid?
+        render :edit
+      end 
+  end
+  private
+  def group_params
+    params.require(:group).permit(:name, {user_ids: []})
+  end
+  
 end
